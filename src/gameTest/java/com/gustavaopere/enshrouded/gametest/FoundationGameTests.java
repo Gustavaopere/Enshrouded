d907bdd23624cd6fdb47ac68679447b7a48f7359
Gustavaopere <gustavaopere@gmail.com>
2026-08-27 12:40:48 -0300

    test: exercise GameTest bootstrap contracts

--- src/gameTest/java/com/gustavaopere/enshrouded/gametest/FoundationGameTests.java
+++ src/gameTest/java/com/gustavaopere/enshrouded/gametest/FoundationGameTests.java
@@ -4,6 +4,10 @@ import com.gustavaopere.enshrouded.Enshrouded;
 import net.minecraft.core.BlockPos;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.decoration.ArmorStand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.GameType;
 import net.minecraft.world.level.block.Blocks;
 import net.neoforged.neoforge.gametest.GameTestHolder;
 import net.neoforged.neoforge.gametest.PrefixGameTestTemplate;
@@ -16,11 +20,25 @@ public final class FoundationGameTests {
 
     @GameTest(template = "foundation_empty")
     public static void foundationTemplateServerLevelAndBlockMutationAreAvailable(GameTestHelper helper) {
-        helper.assertTrue(helper.getLevel() != null, "GameTest must expose a server level");
+        GameTestBootstrap.requireServerLevel(helper);
+        GameTestBootstrap.setAndAssertBlock(helper, new BlockPos(1, 0, 1), Blocks.STONE);
+        helper.succeed();
+    }
 
-        BlockPos mutationPos = new BlockPos(1, 0, 1);
-        helper.setBlock(mutationPos, Blocks.STONE);
-        helper.assertBlockPresent(Blocks.STONE, mutationPos);
+    @GameTest(template = "foundation_empty")
+    public static void foundationPlayerEntityAndSaveFixturesAreAvailable(GameTestHelper helper) {
+        Player player = GameTestBootstrap.makeMockPlayer(helper, GameType.SURVIVAL);
+        player.setHealth(10.0F);
+        helper.assertTrue(player.getHealth() == 10.0F, "Mock player state must be mutable");
+
+        ArmorStand armorStand = GameTestBootstrap.spawnAndRequireAlive(
+                helper,
+                EntityType.ARMOR_STAND,
+                new BlockPos(1, 1, 1));
+        armorStand.setInvisible(true);
+        helper.assertTrue(armorStand.isInvisible(), "Spawned entity state must be mutable");
+
+        GameTestBootstrap.forceSaveForReload(helper);
         helper.succeed();
     }
 }
