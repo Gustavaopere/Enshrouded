821798938666832d80d83a6b310fff7eeb227c42
Gustavaopere <gustavaopere@gmail.com>
2026-08-28 13:31:11 -0300

    feat: add propagation policy contract

---
+++ src/main/java/com/gustavaopere/enshrouded/shroud/expansion/ShroudPropagationPolicy.java
@@ -0,0 +1,7 @@
+package com.gustavaopere.enshrouded.shroud.expansion;
+
+/**
+ * Pure logical propagation seam. Implementations must not load chunks or inspect material blocks.
+ */
+public interface ShroudPropagationPolicy {
+}
