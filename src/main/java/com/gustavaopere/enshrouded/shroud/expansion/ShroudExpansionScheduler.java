0e9f05176cdaad2428e8da86f2dda5a422e21cde
Gustavaopere <gustavaopere@gmail.com>
2026-08-28 13:31:34 -0300

    feat: add expansion scheduler contract

---
+++ src/main/java/com/gustavaopere/enshrouded/shroud/expansion/ShroudExpansionScheduler.java
@@ -0,0 +1,7 @@
+package com.gustavaopere.enshrouded.shroud.expansion;
+
+/**
+ * Coordinates bounded logical frontier work without owning or loading Minecraft chunks.
+ */
+public final class ShroudExpansionScheduler {
+}
