771fe3572a1d278f49e2bd57bc97c1308c16de10
Gustavaopere <gustavaopere@gmail.com>
2026-08-29 06:23:50 -0300

    feat: define exposure reserve defaults

--- src/main/java/com/gustavaopere/enshrouded/exposure/ExposureSchema.java
+++ src/main/java/com/gustavaopere/enshrouded/exposure/ExposureSchema.java
@@ -2,6 +2,8 @@ package com.gustavaopere.enshrouded.exposure;
 
 public final class ExposureSchema {
     public static final int CURRENT_VERSION = 1;
+    public static final int DEFAULT_MAX_RESERVE_TICKS = 300 * 20;
+    public static final int DEFAULT_EMERGENCY_WINDOW_TICKS = 10 * 20;
 
     private ExposureSchema() {
     }
