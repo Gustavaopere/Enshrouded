b912e021982e83212c3ef063ae480de676bcf172
Gustavaopere <gustavaopere@gmail.com>
2026-08-29 14:36:00 -0300

    feat: wire flame-gated deadly runtime

--- src/main/java/com/gustavaopere/enshrouded/exposure/deadly/PassageRequirement.java
+++ src/main/java/com/gustavaopere/enshrouded/exposure/deadly/PassageRequirement.java
@@ -2,9 +2,14 @@ package com.gustavaopere.enshrouded.exposure.deadly;
 
 /** Immutable Flame passage requirement for one Deadly Shroud profile. */
 public record PassageRequirement(int requiredLevel) {
+    public static final int MIN_REQUIRED_LEVEL = 1;
+    public static final int DEFAULT_REQUIRED_LEVEL = 2;
+    public static final int MAX_REQUIRED_LEVEL = 16;
+
     public PassageRequirement {
-        if (requiredLevel <= 0) {
-            throw new IllegalArgumentException("requiredLevel must be > 0");
+        if (requiredLevel < MIN_REQUIRED_LEVEL || requiredLevel > MAX_REQUIRED_LEVEL) {
+            throw new IllegalArgumentException(
+                    "requiredLevel must be within [" + MIN_REQUIRED_LEVEL + "," + MAX_REQUIRED_LEVEL + "]");
         }
     }
 
