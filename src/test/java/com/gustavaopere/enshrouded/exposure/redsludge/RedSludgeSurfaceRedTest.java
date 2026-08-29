317920c3541eae550094e8399222bff7f7e39666
Gustavaopere <gustavaopere@gmail.com>
2026-08-29 15:11:41 -0300

    test: avoid registry bootstrap in red sludge surface check

--- src/test/java/com/gustavaopere/enshrouded/exposure/redsludge/RedSludgeSurfaceRedTest.java
+++ src/test/java/com/gustavaopere/enshrouded/exposure/redsludge/RedSludgeSurfaceRedTest.java
@@ -7,8 +7,9 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 final class RedSludgeSurfaceRedTest {
     @Test
     void plannedRedSludgeTypesExist() throws Exception {
-        assertNotNull(Class.forName("com.gustavaopere.enshrouded.content.fluid.RedSludgeFluid"));
-        assertNotNull(Class.forName("com.gustavaopere.enshrouded.content.fluid.RedSludgeBlock"));
-        assertNotNull(Class.forName("com.gustavaopere.enshrouded.exposure.redsludge.RedSludgeExposureHandler"));
+        ClassLoader loader = RedSludgeSurfaceRedTest.class.getClassLoader();
+        assertNotNull(Class.forName("com.gustavaopere.enshrouded.content.fluid.RedSludgeFluid", false, loader));
+        assertNotNull(Class.forName("com.gustavaopere.enshrouded.content.fluid.RedSludgeBlock", false, loader));
+        assertNotNull(Class.forName("com.gustavaopere.enshrouded.exposure.redsludge.RedSludgeExposureHandler", false, loader));
     }
 }
