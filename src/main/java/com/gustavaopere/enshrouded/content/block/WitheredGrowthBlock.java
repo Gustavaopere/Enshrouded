81a582999577b8ad920e6bf1d3e824ecbb3cc5ad
Gustavaopere <gustavaopere@gmail.com>
2026-08-28 23:15:58 -0300

    feat: add deadly Shroud growth block type

---
+++ src/main/java/com/gustavaopere/enshrouded/content/block/WitheredGrowthBlock.java
@@ -0,0 +1,8 @@
+package com.gustavaopere.enshrouded.content.block;
+
+/** Deadly/Red-Shroud visual growth family. */
+public final class WitheredGrowthBlock extends ShroudGrowthBlock {
+    public WitheredGrowthBlock(Properties properties) {
+        super(properties);
+    }
+}
