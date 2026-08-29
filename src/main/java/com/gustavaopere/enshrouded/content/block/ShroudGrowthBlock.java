2c430056a9d5331f5443a926553536e189ed6874
Gustavaopere <gustavaopere@gmail.com>
2026-08-28 23:15:41 -0300

    feat: add common Shroud growth block type

---
+++ src/main/java/com/gustavaopere/enshrouded/content/block/ShroudGrowthBlock.java
@@ -0,0 +1,10 @@
+package com.gustavaopere.enshrouded.content.block;
+
+import net.minecraft.world.level.block.Block;
+
+/** Common non-container Shroud surface growth family. */
+public class ShroudGrowthBlock extends Block {
+    public ShroudGrowthBlock(Properties properties) {
+        super(properties);
+    }
+}
