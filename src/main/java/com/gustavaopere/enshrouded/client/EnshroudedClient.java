41516a73a8232fa51139ce670bb345905a8ef6a1
Gustavaopere <gustavaopere@gmail.com>
2026-09-06 16:41:09 -0300

    feat: register Flame Altar GeckoLib renderer

--- src/main/java/com/gustavaopere/enshrouded/client/EnshroudedClient.java
+++ src/main/java/com/gustavaopere/enshrouded/client/EnshroudedClient.java
@@ -6,14 +6,17 @@ import com.gustavaopere.enshrouded.client.ambient.ShroudAmbientController;
 import com.gustavaopere.enshrouded.client.effects.ShroudParticleController;
 import com.gustavaopere.enshrouded.client.hud.ShroudHudOverlay;
 import com.gustavaopere.enshrouded.client.render.ShroudFogController;
+import com.gustavaopere.enshrouded.client.render.flame.FlameAltarRenderer;
 import com.gustavaopere.enshrouded.client.state.ClientShroudDiscoveryState;
 import com.gustavaopere.enshrouded.config.EnshroudedClientConfig;
+import com.gustavaopere.enshrouded.registry.ModBlockEntities;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.common.NeoForge;
 
 /** Physical-client-only Stage 07+ bootstrap. */
@@ -24,10 +27,15 @@ public final class EnshroudedClient {
         AccessibilityPresetController.register(modBus);
         modBus.addListener(ShroudHudOverlay::registerGuiLayers);
         modBus.addListener(ShroudParticleController::registerParticleProviders);
+        modBus.addListener(EnshroudedClient::registerRenderers);
         ShroudFogController.register(NeoForge.EVENT_BUS);
         ShroudAmbientController.register(NeoForge.EVENT_BUS);
         ShroudParticleController.register(NeoForge.EVENT_BUS);
         NeoForge.EVENT_BUS.addListener((ClientPlayerNetworkEvent.LoggingOut event) ->
                 ClientShroudDiscoveryState.INSTANCE.reset());
+    }
+
+    private static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerBlockEntityRenderer(ModBlockEntities.FLAME_ALTAR.get(), FlameAltarRenderer::new);
     }
 }
