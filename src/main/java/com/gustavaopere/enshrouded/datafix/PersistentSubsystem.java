0f7feac73a26fd8e62a3d37a428d031071a8f5e3
Gustavaopere <gustavaopere@gmail.com>
2026-09-05 22:05:13 -0300

    feat: include Shroud discovery in centralized migration

--- src/main/java/com/gustavaopere/enshrouded/datafix/PersistentSubsystem.java
+++ src/main/java/com/gustavaopere/enshrouded/datafix/PersistentSubsystem.java
@@ -3,12 +3,14 @@ package com.gustavaopere.enshrouded.datafix;
 import com.gustavaopere.enshrouded.ecology.state.EntityCorruptionSchema;
 import com.gustavaopere.enshrouded.exposure.ExposureSchema;
 import com.gustavaopere.enshrouded.flame.state.FlameProgressionSchema;
+import com.gustavaopere.enshrouded.shroud.discovery.ShroudDiscoverySchema;
 import com.gustavaopere.enshrouded.shroud.state.ShroudSchema;
 import com.gustavaopere.enshrouded.story.state.StorySchema;
 
 /** Persistent Level-1 subsystem formats owned by the centralized Stage 09.03 migration layer. */
 public enum PersistentSubsystem {
     SHROUD("shroud", ShroudSchema.FIRST_VERSION, ShroudSchema.CURRENT_VERSION),
+    SHROUD_DISCOVERY("shroud_discovery", ShroudDiscoverySchema.FIRST_VERSION, ShroudDiscoverySchema.CURRENT_VERSION),
     EXPOSURE("exposure", ExposureSchema.FIRST_VERSION, ExposureSchema.CURRENT_VERSION),
     ENTITY_CORRUPTION("entity corruption", EntityCorruptionSchema.FIRST_VERSION, EntityCorruptionSchema.CURRENT_VERSION),
     FLAME_PROGRESSION("flame_progression", FlameProgressionSchema.FIRST_VERSION, FlameProgressionSchema.CURRENT_VERSION),
