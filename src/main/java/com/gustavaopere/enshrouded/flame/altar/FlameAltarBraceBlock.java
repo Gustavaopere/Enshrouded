673406bb230cdbe45d7ba1462064c3fbba27d858
Gustavaopere <gustavaopere@gmail.com>
2026-09-07 19:16:30 -0300

    feat: orient Flame Altar brace shell

--- src/main/java/com/gustavaopere/enshrouded/flame/altar/FlameAltarBraceBlock.java
+++ src/main/java/com/gustavaopere/enshrouded/flame/altar/FlameAltarBraceBlock.java
@@ -1,21 +1,34 @@
 package com.gustavaopere.enshrouded.flame.altar;
 
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import org.jetbrains.annotations.Nullable;
 
 /** Presentation-only cardinal shell component for the Flame Altar multiblock. */
 public final class FlameAltarBraceBlock extends Block {
     public static final BooleanProperty FORMED = BooleanProperty.create("formed");
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
 
     public FlameAltarBraceBlock(Properties properties) {
         super(properties);
-        registerDefaultState(stateDefinition.any().setValue(FORMED, false));
+        registerDefaultState(stateDefinition.any()
+                .setValue(FORMED, false)
+                .setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    public @Nullable BlockState getStateForPlacement(BlockPlaceContext context) {
+        return defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(FORMED);
+        builder.add(FORMED, FACING);
     }
 }
