123d11ee82fbf74a52caa4be56537dee21d19d01
Gustavaopere <gustavaopere@gmail.com>
2026-09-07 19:58:04 -0300

    feat(stage10.09): unform altar when required rune is removed

--- src/main/java/com/gustavaopere/enshrouded/flame/altar/FlameAltarRuneBlock.java
+++ src/main/java/com/gustavaopere/enshrouded/flame/altar/FlameAltarRuneBlock.java
@@ -1,5 +1,8 @@
 package com.gustavaopere.enshrouded.flame.altar;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -12,6 +15,14 @@ public final class FlameAltarRuneBlock extends Block {
     public FlameAltarRuneBlock(Properties properties) {
         super(properties);
         registerDefaultState(stateDefinition.any().setValue(FORMED, false));
+    }
+
+    @Override
+    protected void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean movedByPiston) {
+        if (state.getBlock() != newState.getBlock() && level instanceof ServerLevel serverLevel) {
+            FlameAltarStructureLifecycle.onRequiredShellRemoved(serverLevel, pos);
+        }
+        super.onRemove(state, level, pos, newState, movedByPiston);
     }
 
     @Override
